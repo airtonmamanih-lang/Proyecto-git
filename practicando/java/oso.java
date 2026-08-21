99f526e8de288d901723340a708c3ab7c1c8c241
Airton <airton.mamanih@unmsm.edu.pe>
2026-08-21 02:27:54 -0500

    Usando listas y arrays

--- practicando/java/oso.java
+++ practicando/java/oso.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
 public class oso {
 
     public static void main(String[] args) {
         System.out.println("Dejame bien oso");
+        String nombre = "oso";
+        System.out.println("El oso se llama: " + nombre);
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("oso");
+        System.out.println("Lista de osos: " + lista);
+
     }
 
 }
