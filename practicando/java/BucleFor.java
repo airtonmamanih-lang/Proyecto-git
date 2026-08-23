a04b69f82d3b264bcdd6b7545e9c861864a18873
Airton <airton.mamanih@unmsm.edu.pe>
2026-08-23 00:41:38 -0500

    Practicando con bucles for y while

---
+++ practicando/java/BucleFor.java
@@ -0,0 +1,15 @@
+public class BucleFor {
+    public static void main(String[] args) {
+
+        System.out.println("\tBucle For");
+        System.out.println("============================");
+
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            System.out.println("\tEl valor de i es: " + i);
+        }
+
+
+
+    }
+}
