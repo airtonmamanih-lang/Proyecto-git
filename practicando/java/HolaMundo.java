0af24b12b3955c391bdeec1c70c525b327aa1f22
Airton <airton.mamanih@unmsm.edu.pe>
2026-08-22 22:23:41 -0500

    Agregar comentarios

--- practicando/java/HolaMundo.java
+++ practicando/java/HolaMundo.java
@@ -2,6 +2,8 @@ public class HolaMundo {
 
     public static void main(String[] args) {
 
+        // Mensajes de java con el crtl + }
+        // Dejame bien mn
         System.out.println("Hola mundo");
         System.out.println("Estoy programando en Java");
         System.out.println("Estoy practicando Java");
