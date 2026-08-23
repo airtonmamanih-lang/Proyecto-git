c9e7b2e434261c203eabdb3c91bb52a72b2f6e6f
Airton <airton.mamanih@unmsm.edu.pe>
2026-08-22 23:30:48 -0500

    Corrigiendo mi public class

---
+++ practicando/java/Condicional.java
@@ -0,0 +1,13 @@
+public class Condicional {
+    public static void main (String[] args) {
+
+        int edad = 17;
+
+        if (edad >= 18) {
+            System.out.println("Eres mayor de edad");
+        } else { 
+            System.out.println("Eres menor de edad bro");
+        }
+
+    }
+}
